2df4eefe27eab2ebea7d37db510bff285c81d93d
Bhuvana Chandrika Kothapalli <Bckothapally@gmail.com>
2023-10-16 21:24:43 -0500

    Create Population Census.sql

---
+++ BASIC JOIN/Population Census.sql
@@ -0,0 +1,11 @@
+-- https://www.hackerrank.com/challenges/asian-population/problem?isFullScreen=true
+
+--Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+-- Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+-- **Input Format**
+-- The CITY and COUNTRY tables are described as follows:
+
+SELECT SUM(CITY.POPULATION)
+FROM CITY
+JOIN COUNTRY ON CITY.COUNTRYCODE=COUNTRY.CODE
+WHERE CONTINENT = 'ASIA';
